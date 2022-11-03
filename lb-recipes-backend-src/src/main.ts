1271871938527c90d7df5b766fc93815ce326c5d
Tom Bury <tom.bury@twipemobile.com>
2022-11-03 12:53:17 +0100

    feat: enable validation

--- lb-recipes-backend-src/src/main.ts
+++ lb-recipes-backend-src/src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -5,6 +6,14 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Remove any properties from request bodies etc that are not defined in the set types
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+
   await app.listen(process.env.PORT || 3000);
 
   if (module.hot) {
