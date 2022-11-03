203b645a963276c1066f311d5b05eb2fb8de55e4
Tom Bury <tom.bury@twipemobile.com>
2022-11-03 21:10:15 +0100

    feat: update docker run script to take version or latest version

--- docker-run.sh
+++ docker-run.sh
@@ -7,11 +7,19 @@ set +a
 VERSION_FILE_PATH="lb-recipes-backend-src/src/config/version.ts"
 CURR_VERSION=$(cat $VERSION_FILE_PATH | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/')
 
+REQUESTED_VERSION=$1
+
+if [ -z "$REQUESTED_VERSION" ]; then
+  REQUESTED_VERSION=$CURR_VERSION
+fi
+
+printf "🏃 Running version v$REQUESTED_VERSION\n\n"
+
 docker run \
   -e TF_VAR_FIREBASE_SA_PRIVATE_KEY="$TF_VAR_FIREBASE_SA_PRIVATE_KEY" \
   -e TF_VAR_FIREBASE_SA_EMAIL=$TF_VAR_FIREBASE_SA_EMAIL \
   -e TF_VAR_GOOGLE_CLOUD_PROJECT_ID=$TF_VAR_GOOGLE_CLOUD_PROJECT_ID \
   -e TF_VAR_ADMIN_PASSWORD=$TF_VAR_ADMIN_PASSWORD \
   -e TF_VAR_AUTH_JWT_SECRET=$TF_VAR_AUTH_JWT_SECRET \
-  -p80:3000 "lb-recipes-backend:v$CURR_VERSION"
+  -p80:3000 "lb-recipes-backend:v$REQUESTED_VERSION"
 
