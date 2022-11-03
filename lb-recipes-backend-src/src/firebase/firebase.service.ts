98327ef66da833f37cc660e4b47a4ef0e60fa6a7
Tom Bury <tom.bury@twipemobile.com>
2022-11-03 12:54:04 +0100

    feat: add storage bucket file uploads

--- lb-recipes-backend-src/src/firebase/firebase.service.ts
+++ lb-recipes-backend-src/src/firebase/firebase.service.ts
@@ -3,10 +3,18 @@ import { ConfigService } from '@nestjs/config';
 import * as admin from 'firebase-admin';
 import { Firestore, getFirestore } from 'firebase-admin/firestore';
 import { Configs } from 'src/config/interfaces/config.interface';
+import { Storage } from '@google-cloud/storage';
+
+const COLLECTIONS = ['lb-recipes', 'lb-recipes-metadata'] as const;
+type TCollectionId = typeof COLLECTIONS[number];
+
+const BUCKETS = ['recipes_thumbs_liesbury-recipes-322314'] as const;
+type TBucketId = typeof BUCKETS[number];
 
 @Injectable()
 export class FirebaseService {
   private db: Firestore;
+  private storage: Storage;
 
   constructor(private readonly configService: ConfigService<Configs, true>) {
     try {
@@ -19,14 +27,30 @@ export class FirebaseService {
         }),
       });
       this.db = getFirestore();
+      this.storage = new Storage();
       this.db.settings({ ignoreUndefinedProperties: true });
     } catch (error) {
       console.error(error);
+      this.storage = new Storage();
       this.db = getFirestore();
     }
   }
 
-  collection(collectionId: string) {
+  collection(collectionId: TCollectionId) {
     return this.db.collection(collectionId);
+  }
+
+  async uploadFile(
+    fileData: Buffer,
+    fileName: string,
+    bucketId: TBucketId,
+  ): Promise<void> {
+    const bucket = this.storage.bucket(bucketId);
+    const newFileRef = bucket.file(fileName);
+    return newFileRef.save(fileData);
+  }
+
+  getStorageFileUrl(fileName: string, bucketId: TBucketId): string {
+    return `https://storage.googleapis.com/${bucketId}/${fileName}`;
   }
 }
