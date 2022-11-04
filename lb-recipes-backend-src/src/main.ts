537d683a5d540eb1d6b6e7766df1d0cc19a538e4
Tom Bury <tom.bury@twipemobile.com>
2022-11-04 13:03:48 +0100

    feat: enable cors for website domain & localhost

--- lb-recipes-backend-src/src/main.ts
+++ lb-recipes-backend-src/src/main.ts
@@ -14,6 +14,18 @@ async function bootstrap() {
     }),
   );
 
+  // CORS
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://recepten.lies.bury.dev',
+      'https://recipes.lies.bury.dev',
+    ],
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    maxAge: 3600,
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT || 3000);
 
   if (module.hot) {
