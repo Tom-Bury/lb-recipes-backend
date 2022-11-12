9baf859a3b97f033cc298a18d0049ce0d79030fa
Tom Bury <tom.bury@twipemobile.com>
2022-11-12 23:04:15 +0100

    feat: add categories module, service & controller

---
+++ lb-recipes-backend-src/src/recipes/categories/categories.service.ts
@@ -0,0 +1,94 @@
+import { Injectable } from '@nestjs/common';
+import { WriteBatch } from 'firebase-admin/firestore';
+import { FirebaseService } from 'src/firebase/firebase.service';
+import { nonNullable } from 'src/utils/validation.utils';
+
+@Injectable()
+export class CategoriesService {
+  constructor(private readonly firebase: FirebaseService) {}
+
+  async getAllNonEmptyCategories(): Promise<
+    {
+      categoryId: string;
+      nbEntries: number;
+    }[]
+  > {
+    const allCategories = (
+      await this.firebase
+        .collection('lb-recipes-categories')
+        .where('nbEntries', '>', 0)
+        .get()
+    ).docs.map((doc) => ({
+      categoryId: doc.id,
+      ...(doc.data() as { nbEntries: number }),
+    }));
+
+    return allCategories;
+  }
+
+  async removeRecipeFromAllCategories(recipeId: string): Promise<void> {
+    const batch = this.firebase.batch();
+    await this.batchRemoveRecipeFromAllCategories(batch, recipeId);
+    await batch.commit();
+  }
+
+  async batchRemoveRecipeFromAllCategories(
+    writeBatch: WriteBatch,
+    recipeId: string,
+  ): Promise<void> {
+    const categoriesCol = this.firebase.collection('lb-recipes-categories');
+    const allCategories = (await categoriesCol.get()).docs.map((doc) => doc.id);
+
+    const categoriesContainingRecipe = (
+      await Promise.all(
+        allCategories.map(
+          (category) =>
+            new Promise<string | undefined>((resolve) => {
+              categoriesCol
+                .doc(category)
+                .collection('entries')
+                .doc(recipeId)
+                .get()
+                .then((doc) => {
+                  if (doc.exists) {
+                    resolve(category);
+                  } else {
+                    resolve(undefined);
+                  }
+                });
+            }),
+        ),
+      )
+    ).filter(nonNullable);
+
+    categoriesContainingRecipe.forEach((category) => {
+      writeBatch.delete(
+        categoriesCol.doc(category).collection('entries').doc(recipeId),
+      );
+      writeBatch.update(categoriesCol.doc(category), {
+        nbEntries: FirebaseService.DECREMENT,
+      });
+    });
+  }
+
+  batchWriteRecipeCategories(
+    writeBatch: WriteBatch,
+    recipeId: string,
+    categories: string[] | undefined,
+  ): void {
+    if (!categories || categories.length === 0) return;
+
+    const categoriesCol = this.firebase.collection('lb-recipes-categories');
+    categories.forEach((category) => {
+      writeBatch.set(
+        categoriesCol.doc(category).collection('entries').doc(recipeId),
+        {
+          recipeId,
+        },
+      );
+      writeBatch.set(categoriesCol.doc(category), {
+        nbEntries: FirebaseService.INCREMENT,
+      });
+    });
+  }
+}
