65f0bfda36dae0d5042a7364307b89d33f660cca
Tom Bury <tom.bury@twipemobile.com>
2023-03-18 11:45:33 +0100

    feat: add DB export backup bucket

--- lb-recipes-backend-infr/storage.tf
+++ lb-recipes-backend-infr/storage.tf
@@ -8,3 +8,11 @@ module "recipes_images_bucket" {
   service_account_role = "roles/storage.objectAdmin"
   public = true
 }
+
+# Database exports bucket
+resource "google_storage_bucket" "db_exports_bucket" {
+  name = "lb_recipes_db_exports"
+  location = "EUROPE-WEST1"
+  force_destroy = false
+  uniform_bucket_level_access = true
+}
