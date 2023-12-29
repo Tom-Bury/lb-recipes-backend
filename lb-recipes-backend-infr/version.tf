a618efaa2ece9b3a5a7103d40907fefdeb3d803a
Tom Bury <tom.bury@twipemobile.com>
2023-12-29 15:20:56 +0100

    ci: version bump v0.2.1

--- lb-recipes-backend-infr/version.tf
+++ lb-recipes-backend-infr/version.tf
@@ -1,3 +1,3 @@
 variable "container_img" {
-  default = "europe-west1-docker.pkg.dev/liesbury-recipes-322314/lb-recipes-artifact-registry/lb-recipes-backend:v0.2.0"
+  default = "europe-west1-docker.pkg.dev/liesbury-recipes-322314/lb-recipes-artifact-registry/lb-recipes-backend:v0.2.1"
 }
