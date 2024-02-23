88c08056a93ec84277cc50bc3eb055f515cd422d
Tom Bury <tom.bury@twipemobile.com>
2024-02-23 23:07:29 +0100

    feat: update storage bucket and remove firstore db export bucket

--- lb-recipes-backend-infr/storage.tf
+++ lb-recipes-backend-infr/storage.tf
@@ -1,18 +1,10 @@
 # Recipe thumbs bucket
 module "recipes_images_bucket" {
-  source = "./modules/service-account-bucket"
-  bucket_name = "lb_recipes_previews_${var.GOOGLE_CLOUD_PROJECT_ID}"
+  source      = "./modules/service-account-bucket"
+  bucket_name = "lb-recipes-previews-${var.GOOGLE_CLOUD_PROJECT_ID}"
   service_account_emails = [
-    google_service_account.sa_recipes_service.email,
+    google_service_account.lb_recipes_backend_cloud_run_sa.email,
   ]
   service_account_role = "roles/storage.objectAdmin"
-  public = true
-}
-
-# Database exports bucket
-resource "google_storage_bucket" "db_exports_bucket" {
-  name = "lb_recipes_db_exports"
-  location = "EUROPE-WEST1"
-  force_destroy = false
-  uniform_bucket_level_access = true
+  public               = true
 }
