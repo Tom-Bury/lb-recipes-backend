73c1d508dade5f56898fad35d612900aaf332855
Tom Bury <tom.bury@twipemobile.com>
2024-02-23 23:37:45 +0100

    fix: add credentials block to backend

--- lb-recipes-backend-infr/backend.tf
+++ lb-recipes-backend-infr/backend.tf
@@ -3,5 +3,6 @@ terraform {
   backend "gcs" {
     bucket = "liesbury-recipes-gcp-terraform-state"
     prefix = "terraform/state"
+    credentials = var.TERRAFORM_SA_CREDS_FILE_PATH # TODO: fill in manually, but don't commit. Variables cannot be used here 
   }
 }
