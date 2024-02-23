3d8b7295708c6408ae253e755587ca775e2c7c00
Tom Bury <tom.bury@twipemobile.com>
2024-02-23 23:05:59 +0100

    feat: rework cloud_run

--- lb-recipes-backend-infr/cloud_run.tf
+++ lb-recipes-backend-infr/cloud_run.tf
@@ -1,74 +1,72 @@
 # Enables the Cloud Run API
 resource "google_project_service" "run_api" {
-  service = "run.googleapis.com"
-
+  service            = "run.googleapis.com"
   disable_on_destroy = true
 }
 
 # Service account for the cloud run instance
-resource "google_service_account" "sa_recipes_service" {
-  account_id   = "sa-lb-recipes-backend"
-  display_name = "sa-lb-recipes-backend"
+resource "google_service_account" "lb_recipes_backend_cloud_run_sa" {
+  account_id   = "lb-recipes-backend-cloud-run-sa"
+  display_name = "lb-recipes-backend-cloud-run-sa"
 }
 
 # Ensure service account can access FireStore
-resource google_project_iam_member "firestore_user" {
+resource "google_project_iam_member" "firestore_user" {
   project = var.GOOGLE_CLOUD_PROJECT_ID
-  role   = "roles/datastore.user"
-  member = "serviceAccount:${google_service_account.sa_recipes_service.email}"
+  role    = "roles/datastore.user"
+  member  = "serviceAccount:${google_service_account.lb_recipes_backend_cloud_run_sa.email}"
 }
 
-# Create the Cloud Run service
-resource "google_cloud_run_service" "recipes_service" {
-  name = "lb-recipes-backend"
-  location = var.region
+# # Create the Cloud Run service
+# resource "google_cloud_run_service" "lb_recipes_backend_cloud_run_service" {
+#   name = "lb-recipes-backend"
+#   location = var.GOOGLE_CLOUD_REGION
 
-  template {
-    spec {
-      timeout_seconds = 30
-      service_account_name = google_service_account.sa_recipes_service.email
+#   template {
+#     spec {
+#       timeout_seconds = 30
+#       service_account_name = google_service_account.lb_recipes_backend_cloud_run_sa.email
 
-      containers {
-        image = var.container_img
+#       containers {
+#         image = var.container_img
 
-        resources {
-          limits = {
-            cpu = "1000m"
-            memory = "1024Mi"
-          }
-        }
+#         resources {
+#           limits = {
+#             cpu = "1000m"
+#             memory = "1024Mi"
+#           }
+#         }
 
-        env {
-          name = "TF_VAR_GOOGLE_CLOUD_PROJECT_ID"
-          value = var.GOOGLE_CLOUD_PROJECT_ID
-        }
+#         env {
+#           name = "TF_VAR_GOOGLE_CLOUD_PROJECT_ID"
+#           value = var.GOOGLE_CLOUD_PROJECT_ID
+#         }
 
-        env {
-          name = "TF_VAR_ADMIN_PASSWORD"
-          value = var.ADMIN_PASSWORD
-        }
+#         env {
+#           name = "TF_VAR_ADMIN_PASSWORD"
+#           value = var.ADMIN_PASSWORD
+#         }
 
-        env {
-          name = "TF_VAR_AUTH_JWT_SECRET"
-          value = var.AUTH_JWT_SECRET
-        }
-      }
-    }
-  }
+#         env {
+#           name = "TF_VAR_AUTH_JWT_SECRET"
+#           value = var.AUTH_JWT_SECRET
+#         }
+#       }
+#     }
+#   }
 
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
+#   traffic {
+#     percent         = 100
+#     latest_revision = true
+#   }
 
-  # Waits for the Cloud Run API to be enabled
-  depends_on = [google_project_service.run_api]
-}
+#   # Waits for the Cloud Run API to be enabled
+#   depends_on = [google_project_service.run_api]
+# }
 
-# Make recipes_service publicly accessible
-resource "google_cloud_run_service_iam_member" "recipes_service_public" {
-  service  = google_cloud_run_service.recipes_service.name
-  location = google_cloud_run_service.recipes_service.location
-  role     = "roles/run.invoker"
-  member   = "allUsers"
-}
+# # Make recipes_service publicly accessible
+# resource "google_cloud_run_service_iam_member" "lb_recipes_backend_public" {
+#   service  = google_cloud_run_service.lb_recipes_backend_cloud_run_service.name
+#   role     = "roles/run.invoker"
+#   member   = "allUsers"
+# }
