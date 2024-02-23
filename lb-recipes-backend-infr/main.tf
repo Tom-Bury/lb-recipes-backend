d18a9eef94c91e46f5b083c458c2b7689b00ed13
Tom Bury <tom.bury@twipemobile.com>
2024-02-23 23:05:35 +0100

    feat: update provider

--- lb-recipes-backend-infr/main.tf
+++ lb-recipes-backend-infr/main.tf
@@ -1,10 +1,19 @@
-# main.tf
-
 terraform {
   required_version = ">= 0.14"
 
   required_providers {
-    # Cloud Run support was added on 3.3.0
-    google = ">= 3.3"
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
   }
+}
+
+provider "google" {
+  project = var.GOOGLE_CLOUD_PROJECT_ID
+
+  region = var.GOOGLE_CLOUD_REGION
+  zone   = var.GOOGLE_CLOUD_ZONE
+
+  credentials = file(var.TERRAFORM_SA_CREDS_FILE_PATH)
 }
--- lb-recipes-backend-infr/variables.tf
+++ lb-recipes-backend-infr/variables.tf
@@ -1,11 +1,13 @@
 variable "GOOGLE_CLOUD_PROJECT_ID" {}
 
+variable "GOOGLE_CLOUD_REGION" {}
+
+variable "GOOGLE_CLOUD_ZONE" {}
+
+variable "TERRAFORM_SA_CREDS_FILE_PATH" {}
+
 variable "TERRAFORM_STATE_BUCKET_NAME" {}
 
 variable "ADMIN_PASSWORD" {}
 
 variable "AUTH_JWT_SECRET" {}
-
-variable "region" {
-	default = "europe-west1" 
-}
