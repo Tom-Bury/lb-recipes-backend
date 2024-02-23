a876893862b50d51c4ff6a30febaad2567d0c987
Tom Bury <tom.bury@twipemobile.com>
2024-02-23 23:12:16 +0100

    chore: remove unused TERRAFORM_STATE_BUCKET_NAME variable

--- lb-recipes-backend-infr/variables.tf
+++ lb-recipes-backend-infr/variables.tf
@@ -6,8 +6,6 @@ variable "GOOGLE_CLOUD_ZONE" {}
 
 variable "TERRAFORM_SA_CREDS_FILE_PATH" {}
 
-variable "TERRAFORM_STATE_BUCKET_NAME" {}
-
 variable "ADMIN_PASSWORD" {}
 
 variable "AUTH_JWT_SECRET" {}
