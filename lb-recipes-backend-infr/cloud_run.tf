f6e7791edaf37cef00ca359085cc60b99f452e6f
Tom Bury <tom.bury@twipemobile.com>
2024-02-26 23:03:30 +0100

    feat: migrate to google_cloud_run_v2_service

--- lb-recipes-backend-infr/cloud_run.tf
+++ lb-recipes-backend-infr/cloud_run.tf
@@ -3,7 +3,7 @@ resource "google_project_service" "run_api" {
   service            = "run.googleapis.com"
   disable_on_destroy = true
 
-  depends_on = [ google_project_service.service_usage_api ]
+  depends_on = [google_project_service.service_usage_api]
 }
 
 # Service account for the cloud run instance
@@ -22,46 +22,46 @@ resource "google_project_iam_member" "firestore_user" {
 }
 
 # Create the Cloud Run service
-resource "google_cloud_run_service" "lb_recipes_backend_cloud_run_service" {
-  name = "lb-recipes-backend"
+resource "google_cloud_run_v2_service" "lb_recipes_backend_cloud_run_service" {
+  name     = "lb-recipes-backend"
   location = var.GOOGLE_CLOUD_REGION
 
   template {
-    spec {
-      timeout_seconds = 30
-      service_account_name = google_service_account.lb_recipes_backend_cloud_run_sa.email
+    timeout         = "30s"
+    service_account = google_service_account.lb_recipes_backend_cloud_run_sa.email
 
-      containers {
-        image = local.container_img
+    containers {
+      image = local.container_img
 
-        resources {
-          limits = {
-            cpu = "1000m"
-            memory = "1024Mi"
-          }
-        }
-
-        env {
-          name = "TF_VAR_GOOGLE_CLOUD_PROJECT_ID"
-          value = var.GOOGLE_CLOUD_PROJECT_ID
-        }
-
-        env {
-          name = "TF_VAR_ADMIN_PASSWORD"
-          value = var.ADMIN_PASSWORD
-        }
-
-        env {
-          name = "TF_VAR_AUTH_JWT_SECRET"
-          value = var.AUTH_JWT_SECRET
+      resources {
+        limits = {
+          cpu      = "1"
+          memory   = "1Gi"
+          cpu_idle = true
         }
       }
+
+      env {
+        name  = "TF_VAR_GOOGLE_CLOUD_PROJECT_ID"
+        value = var.GOOGLE_CLOUD_PROJECT_ID
+      }
+
+      env {
+        name  = "TF_VAR_ADMIN_PASSWORD"
+        value = var.ADMIN_PASSWORD
+      }
+
+      env {
+        name  = "TF_VAR_AUTH_JWT_SECRET"
+        value = var.AUTH_JWT_SECRET
+      }
     }
+
   }
 
   traffic {
-    percent         = 100
-    latest_revision = true
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
   }
 
   # Waits for the Cloud Run API to be enabled
@@ -70,7 +70,7 @@ resource "google_cloud_run_service" "lb_recipes_backend_cloud_run_service" {
 
 # Make recipes_service publicly accessible
 resource "google_cloud_run_service_iam_member" "lb_recipes_backend_public" {
-  service  = google_cloud_run_service.lb_recipes_backend_cloud_run_service.name
-  role     = "roles/run.invoker"
-  member   = "allUsers"
+  service = google_cloud_run_service.lb_recipes_backend_cloud_run_service.name
+  role    = "roles/run.invoker"
+  member  = "allUsers"
 }
