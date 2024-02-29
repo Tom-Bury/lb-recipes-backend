9b62e5d3aa7083c780e767dd6ecfda4a8f2855c5
Tom Bury <tom.bury@twipemobile.com>
2024-02-29 08:33:31 +0100

    feat: add github actions service account

--- lb-recipes-backend-infr/artifact_registry.tf
+++ lb-recipes-backend-infr/artifact_registry.tf
@@ -16,3 +16,22 @@ resource "google_artifact_registry_repository" "lb_recipes_docker_container_repo
   # Waits for the Artifact Registry  API to be enabled
   depends_on = [google_project_service.artifact_registry_api]
 }
+
+resource "google_service_account" "github_actions_sa" {
+  account_id   = "github-actions-sa"
+  display_name = "github-actions-sa"
+
+  depends_on = [google_project_service.iam_api]
+}
+
+resource "google_project_iam_member" "github_actions_sa_artifact_registry" {
+  project = var.GOOGLE_CLOUD_PROJECT_ID
+  role    = "roles/artifactregistry.writer"
+  member  = "serviceAccount:${google_service_account.github_actions_sa.email}"
+}
+
+resource "google_project_iam_member" "github_actions_sa_token_creator" {
+  project = var.GOOGLE_CLOUD_PROJECT_ID
+  role    = "roles/iam.serviceAccountTokenCreator"
+  member  = "serviceAccount:${google_service_account.github_actions_sa.email}"
+}
