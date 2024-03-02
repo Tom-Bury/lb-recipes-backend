12dfa1ddec4995656e36a79b92db11d7d7cd36c4
Tom Bury <tom.bury@twipemobile.com>
2024-03-02 18:00:08 +0100

    chore: minor version bump

--- lb-recipes-backend-infr/container_image.tf
+++ lb-recipes-backend-infr/container_image.tf
@@ -1,3 +1,3 @@
 locals {
-  container_img = "europe-west1-docker.pkg.dev/${var.GOOGLE_CLOUD_PROJECT_ID}/lb-recipes-docker-container-repo/lb-recipes-backend:v1.1.1"
+  container_img = "europe-west1-docker.pkg.dev/${var.GOOGLE_CLOUD_PROJECT_ID}/lb-recipes-docker-container-repo/lb-recipes-backend:v1.2.0"
 }
